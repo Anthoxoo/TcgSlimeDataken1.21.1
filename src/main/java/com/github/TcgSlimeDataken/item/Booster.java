d804946a2a0dd68d0a843be0717be4a797b3bd47
Chop <melkior.rohart@gmail.com>
2026-03-27 21:48:17 +0100

    test card

--- src/main/java/com/github/TcgSlimeDataken/item/Booster.java
+++
@@ -1,4 +0,0 @@
-package com.github.TcgSlimeDataken.item;
-// This class will contain all the methods needed for the booster item.
-public class Booster {
-}
---
+++ src/main/java/com/github/TcgSlimeDataken/item/CardTestItem.java
@@ -0,0 +1,14 @@
+package com.github.TcgSlimeDataken.item;
+
+import net.minecraft.world.item.Item;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+
+public class CardTestItem {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems("simplecards");
+
+    public static final DeferredHolder<Item, Item> CARTE_STANDARD = ITEMS.register("carte_standard",
+            () -> new Item(new Item.Properties().stacksTo(64))
+    );
+}
