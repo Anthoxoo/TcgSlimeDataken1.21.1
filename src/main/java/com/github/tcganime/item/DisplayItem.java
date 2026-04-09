d4ea0adf083289faa597b850bf3436500f650d06
Antoine <briocheguenot@gmail.com>
2026-04-09 15:37:39 +0200

    added on use method for display and bone for pullRandomGodPack and pullRandomBooster

--- src/main/java/com/github/tcganime/item/DisplayItem.java
+++ src/main/java/com/github/tcganime/item/DisplayItem.java
@@ -1,6 +1,6 @@
 package com.github.tcganime.item;
 
-import com.github.tcganime.services.CardServices;
+import com.github.tcganime.services.BoosterServices;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
@@ -8,6 +8,8 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.neoforged.neoforge.items.ItemHandlerHelper;
+
+import java.util.Random;
 
 public class DisplayItem extends Item {
     private final String name;
@@ -25,18 +27,34 @@ public class DisplayItem extends Item {
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
         ItemStack displayStack = player.getItemInHand(hand);
 
+
         if (!level.isClientSide()) {
-            ItemStack booster1 = CardServices.pullRandomCard().toStack();
-            ItemStack card2 = CardServices.pullRandomCard().toStack();
-            ItemStack card3 = CardServices.pullRandomCard().toStack();
 
-            ItemHandlerHelper.giveItemToPlayer(player, card1);
-            ItemHandlerHelper.giveItemToPlayer(player, card2);
-            ItemHandlerHelper.giveItemToPlayer(player, card3);
+            int numberGodPack;
 
-            boosterStack.shrink(1);
+            Random rand = new Random();
+            int randomIndex = rand.nextInt(10);
+
+            numberGodPack = switch (randomIndex) {
+                case 0, 1, 2, 3, 4, 5 -> 1;
+                case 6, 7, 8 -> 2;
+                default -> 3;
+            };
+            int numberBoosterPerDisplay = 6 - numberGodPack;
+
+            for (int i = 0; i < numberBoosterPerDisplay; i++) {
+                ItemStack pulledBooster = BoosterServices.pullRandomBooster().toStack();
+                ItemHandlerHelper.giveItemToPlayer(player, pulledBooster);
+            }
+
+            for (int i = 0; i < numberGodPack; i++) {
+                ItemStack pulledBooster = BoosterServices.pullRandomGodPack().toStack();
+                ItemHandlerHelper.giveItemToPlayer(player, pulledBooster);
+            }
+
+            displayStack.shrink(1);
         }
 
-        return InteractionResultHolder.sidedSuccess(boosterStack, level.isClientSide());
+        return InteractionResultHolder.sidedSuccess(displayStack, level.isClientSide());
     }
 }
---
+++ src/main/java/com/github/tcganime/services/BoosterServices.java
@@ -0,0 +1,15 @@
+package com.github.tcganime.services;
+
+import com.github.tcganime.item.BoosterItem;
+import net.neoforged.neoforge.registries.DeferredItem;
+
+public class BoosterServices {
+
+    public static DeferredItem<BoosterItem> pullRandomBooster() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public static DeferredItem<BoosterItem> pullRandomGodPack() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
